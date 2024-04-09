5347a9ee790689b6a3e62f625a0ac69c23363c24
Praveen Kumar Gupta <62584529+psgpraveen@users.noreply.github.com>
2024-04-09 23:03:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const PORT = process.env.PORT || 5000
 app.use(express.json()) 
 app.use(cors())
+app.use('./backend/app',router)
 app.get("/status", async (req, res) => {
   let data=await status.find({});
   res.send(data);
